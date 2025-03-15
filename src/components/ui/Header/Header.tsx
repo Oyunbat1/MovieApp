61d98a12ddc7fed151b3e38eed51712f0bde0458
Oyunbat <oyunbat9958@gmail.com>
2025-03-15 17:30:46 +0800

    header style is almost done

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,42 +1,128 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import First from './firstStep/First'
-import Second from './secondStep/Second'
+"use client";
+import React from "react";
+import Image from "next/image";
+import First from "./firstStep/First";
+import Second from "./secondStep/Second";
 import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button"
-import { Search } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { useMediaQuery } from "react-responsive";
+import { Search, Moon, ChevronDown } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
 
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+type Checked = DropdownMenuCheckboxItemProps["checked"];
 
 function Header() {
+  const [search, setSearch] = useState(null);
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false);
+  const [showPanel, setShowPanel] = React.useState<Checked>(false);
+  const isMobileQuery = useMediaQuery({ maxWidth: 639 });
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    setIsMobile(isMobileQuery);
+  }, [isMobileQuery]);
 
-    const [search , setSearch] = useState(null)
+  // const directSearch = () => {
+  //   setSearch("directSearch");
+  // };
 
-    const searchFirst = ()=>{ 
-        setSearch("first")
-    }
-    const searchSecond = ()=>{  
-      setSearch("second")
-    }
   return (
-    <div className='h-[60px] bg-white w-full flex items-center justify-around gap-20 sm:bg-amber-500  md:bg-blue-500  lg:bg-green-500  xl:bg-pink-500'>
-       
-        {search === null && (
-        <div className="w-full  flex justify-around sm:bg-amber-500 md:bg-blue-500 lg:bg-green-500 xl:bg-pink-500">
-          <Image src="/Logo.png" width={100} height={50} alt='Movie Logo'/>
-      <div>
-      <Button className='w-[35px] text-[10px] bg-white text-black ' onClick={searchFirst}><Search /></Button>
-       
-       <Button className='w-[35px] text-[10px] bg-white text-black ' onClick={searchFirst}><Search /></Button>
-      </div>
+    <div>
+      {isMobile && (
+        <div className="h-[60px] bg-white flex justify-center w-full items-center gap-20 ">
+          {search === null && (
+            <div className="w-full  flex justify-around gap-20 items-center   ">
+              <Image
+                src="/Logo.png"
+                width={120}
+                height={50}
+                alt="Movie Logo"
+                className="mt-[10px]"
+              />
+              <div className="flex gap-4 mt-[10px]">
+                <Button
+                  // onClick={directSearch}
+                  className="w-[35px] text-[10px] bg-white text-black hover:text-white "
+                >
+                  <Search />
+                </Button>
+                <Button className="w-[35px] text-[10px] bg-white text-black  hover:text-white">
+                  <Moon />
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
       )}
-        <div className='w-full'>
-            {search === "first" && <First/> }
-            {search === "second" && <Second/> }
+      {!isMobile && (
+        <div className="h-[80px] bg-white flex justify-center w-full items-center gap-20 ">
+          {search === null && (
+            <div className="w-full  flex justify-around gap-20 sm:gap-4 xl:gap-40 items-center  ">
+              <Image src="/Logo.png" width={120} height={50} alt="Movie Logo" />
+              <div className="flex gap-4">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className="w-[97px]">
+                      <ChevronDown />
+                      Genre
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem
+                      checked={showStatusBar}
+                      onCheckedChange={setShowStatusBar}
+                    >
+                      Status Bar
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                      checked={showActivityBar}
+                      onCheckedChange={setShowActivityBar}
+                      disabled
+                    >
+                      Activity Bar
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                      checked={showPanel}
+                      onCheckedChange={setShowPanel}
+                    >
+                      Panel
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <div
+                  className="flex bg-white md:w-[379px]  px-4 rounded-md
+               items-center border  border-solid shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)]"
+                >
+                  <Search className="text-[#E4E4E7]" />
+                  <Input
+                    type="text"
+                    placeholder="Search..."
+                    className="border-none outline-none focus:outline-none "
+                  />
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <Button className="w-[35px] text-[10px] bg-white text-black  hover:text-white">
+                  <Moon />
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/ui/Header/firstStep/First.tsx
+++ src/components/ui/Header/firstStep/First.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import { Search, X } from "lucide-react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
 function First() {
   return (
-    <div>
-      First
+    <div className="flex">
+      <Image
+        src="/Logo.png"
+        width={120}
+        height={50}
+        alt="Movie Logo"
+        className="mt-[10px]"
+      />
+      <div
+        className="flex bg-white md:w-[379px]  px-4 rounded-md
+               items-center border  border-solid shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)]"
+      >
+        <Search className="text-[#E4E4E7]" />
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="border-none outline-none focus:outline-none "
+        />
+        <X />
+      </div>
     </div>
-  )
+  );
 }
 
-export default First
+export default First;
