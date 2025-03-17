00741e8e15f9de38ed84983c1eca64feb4f78ece
24LP1447 24LP1447 <24LP1447@G303-CC0137.local>
2025-03-17 12:17:14 +0800

    detail hesegru api damjulsan , jujigchdin api bolon trailer hesgiinh dutuu

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -4,16 +4,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import axios from "axios";
 import ACCESS_TOKEN from "@/constants/index";
-
+import Movie from "@/components/type/Type";
+import Header from "@/components/ui/Header/Header";
+import FirstStep from "@/components/ui/Header/firstStep/First";
 export default function MovieDetailPage() {
   const { id } = useParams();
-  
-console.log("Movie ID:", id);
+
+  console.log("Movie ID:", id);
 
   const [movie, setMovie] = useState(null);
+  console.log(movie);
 
-//   console.log(movie)
-    
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
@@ -36,14 +37,32 @@ console.log("Movie ID:", id);
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold">{movie.title}</h1>
-      {/* <img
+      <div>{/* <Header/> */}</div>
+      <div className="flex justify-around gap-8">
+        <div>
+          {" "}
+          <h1 className="text-3xl font-bold">{movie.title}</h1>
+          <p className="text-1xl font-bold">{movie.release_date}</p>
+        </div>
+        <div>
+          {" "}
+          <p className="mt-2">⭐ {movie.vote_average}/10</p>
+        </div>
+      </div>
+        <div>Trailer</div>
+    <div className="flex gap-4">
+    <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
-        className="mt-4 rounded-lg"
+        className="mt-4  w-[100px] h-[148px]"
       />
-      <p className="mt-4 text-lg">{movie.overview}</p>
-      <p className="mt-2">⭐ {movie.vote_average}/10</p> */}
+     <div>
+    {movie.genres.map((genre) => (
+        <p key={genre.id}>{genre.name}</p>
+    ))}
+     <p className="mt-4 text-lg">{movie.overview}</p>
+     </div>
+    </div>
     </div>
   );
 }
